efe20f382fc001e69431b8fd02fdc79fa2d1786d
LlamameAsh <JossCuriel117@gmail.com>
2020-11-19 17:32:59 -0600

    Creación de la clase Empleado.

---
+++ src/com/poo/practica7/Empleado.java
@@ -0,0 +1,10 @@
+package com.poo.practica7;
+
+public abstract class Empleado {
+    String nombre;
+    int ID;
+    double salario;
+    int horasDeTrabajo;
+
+    public abstract int trabajar();
+}
